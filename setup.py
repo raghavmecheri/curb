1f575510e6f997fe5c5dd8c2aa2e9557f72ab2e6
Raghav Mecheri <raghav.mecheri@columbia.edu>
2021-10-24 01:05:52 -0400

    Modified setup.py to make curb a fully fledged shell command

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 """setup file for curb deployments
 """
-__version__ = "0.0.4"
-__test_version__ = "0.0.4"
+__version__ = "0.0.5"
+__test_version__ = "0.0.5"
 
+from setuptools import setup, find_packages
+from io import open
 from os import path, getenv
-from setuptools import setup
+
+import pathlib
 
 version = __test_version__ if getenv("TESTBUILD") == "True" else __version__
-
 base_url, package_name = "https://github.com/raghavmecheri/curb", "curb"
 
 this_directory = path.abspath(path.dirname(__file__))
@@ -20,13 +22,19 @@ setup(
     description="A minimalist module to containerize child processes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/raghavmecheri/{}".format(package_name),
+    packages=find_packages(),  # list of all packages
+    install_requires=["click==8.0.1", "psutil==5.8.0"],
+    python_requires=">=3.6",
+    entry_points="""
+        [console_scripts]
+        curb=curb.__main__:main
+        """,
     author="Raghav Mecheri",
     author_email="raghav@binit.in",
     license="MIT",
     download_url="{0}/archive/{1}-{2}".format(base_url, package_name, version),
-    packages=[package_name],
-    install_requires=["click==8.0.1", "psutil==5.8.0"],
+    keyword="curb, container, resource",
+    url="https://github.com/raghavmecheri/{}".format(package_name),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
